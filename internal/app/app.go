bfcbb744f8ad45e695d521fb394220a5ae298897
moatassem47 <199459806+moatassem47@users.noreply.github.com>
2026-07-08 09:10:39 +0300

    Add Database in Docker

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,27 +1,33 @@
 package app
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/moatassem47/back-end-project/internal/api"
+	"github.com/moatassem47/back-end-project/internal/store"
 )
 
 type Application struct {
 	Logger         *log.Logger
 	WorkoutHandler *api.WorkoutHandler
+	DB             *sql.DB
 }
 
 func NewApplication() (*Application, error) {
+	pgDB, err := store.Open()
+	if err != nil {
+		fmt.Printf("error:%v", err)
+	}
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
-	//our stores will go here
 
-	// our handlers will go here
 	workoutHandler := api.NewWorkoutHandler()
 	app := &Application{Logger: logger,
 		WorkoutHandler: workoutHandler,
+		DB:             pgDB,
 	}
 
 	return app, nil
